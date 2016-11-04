29c0994869581d4b07d00e710017070025031a92
Tadas Plančiūnas <tadas.planciunas@ferratum.com>
2016-11-04 09:41:38 +0100

    Missing controller.

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * BComeSafe, http://bcomesafe.com
+ * Copyright 2015 Magenta ApS, http://magenta.dk
+ * Licensed under MPL 2.0, https://www.mozilla.org/MPL/2.0/
+ * Developed in co-op with Baltic Amadeus, http://baltic-amadeus.lt
+ */
+
+namespace BComeSafe\Http\Controllers;
+
+class TestController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function getIndex()
+    {
+        //        dd(\Auth::user());
+        return redirect('/');
+    }
+}
