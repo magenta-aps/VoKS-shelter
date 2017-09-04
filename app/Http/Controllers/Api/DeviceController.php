e1455cf91467d800b5199dfaa2b4af5b9fa8f649
Tadas Plančiūnas <tadas.planciunas@ferratum.com>
2017-09-04 15:10:51 +0300

    API:BCS:LIST

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -251,7 +251,7 @@ class DeviceController extends Controller
     public function anyList(BcsRequest $request)
     {
         $ret_val = array();
-        $list = School::where('display', '=', '1')->toArray();
+        $list = School::where('display', '=', '1')->get()->toArray();
         if (empty($list)) {
           return response()->json($ret_val);
         }
