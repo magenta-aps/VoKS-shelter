cd0b4b6bfe62628358619bb8589c1bff9e620167
Tadas Planciunas <tadas.planciunas@gmail.com>
2019-01-21 14:15:19 +0200

    Debug

--- resources/views/shelter/views/app-stream-html.blade.php
+++ resources/views/shelter/views/app-stream-html.blade.php
@@ -11,10 +11,10 @@
             </div>
             <div class="streams__video-box">
                 <div class="streams__video-tag-block">
-                    <video id="streams_video-element_<% client.connection.clientId %>" class="streams__video-element" ng-src="<% client.stream.url | trusted %>" listen="client.state.muted" autoplay></video>
+                    <video id="streams_video-element_<% client.profile.id %>" class="streams__video-element" ng-src="<% client.stream.url | trusted %>" listen="client.state.muted" autoplay></video>
                     <script type="text/javascript">
-                      var videoTag = document.getElementById('streams_video-element_<% client.profile.id %>');
-                      //.strObject = <% client.stream.object %>;
+                      var videoTag = document.getElementById('streams_video-element_' + <% client.profile.id %>);
+                      console.log(<% client.stream %>);
                       console.log(videoTag);
                     </script>
                 </div>
