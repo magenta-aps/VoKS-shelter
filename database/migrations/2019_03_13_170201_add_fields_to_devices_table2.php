30b8ad32d6c350a029a7141b92fc4f0df5ca1390
Tadas Planciunas <tadas.planciunas@gmail.com>
2019-03-14 15:17:03 +0200

    Migrations.

--- database/migrations/2019_03_13_170201_add_fields_to_devices_table2.php
+++ database/migrations/2019_03_13_170201_add_fields_to_devices_table2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToDevicesTable extends Migration {
+class AddFieldsToDevicesTable2 extends Migration {
 
 	/**
 	 * Run the migrations.
