68e705501ffaf8d0ae3db5b88bb39c260d6ce413
Tadas Planciunas <tadas.planciunas@gmail.com>
2019-03-14 14:21:23 +0200

    Update profile

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -162,7 +162,7 @@ class DeviceController extends Controller
         }
 
         $device = Device::updateOnTrigger($device);
-
+        
         $status = (int)\Shelter::getInitiationStatus($device['school_id']);
 
         // update shelter activity timestamp
@@ -183,18 +183,21 @@ class DeviceController extends Controller
 
         switch ($device['trigger_status']) {
             case Device::TRIGGERED:
-                // if it's the first trigger, fire up the events
-                if (!$device['already_triggered'] && $status === Device::TRIGGERED) {
-                    \Event::fire(new AlarmWasTriggered($device['school_id']));
-                    //Set School status
-                    SchoolStatus::statusAlarm($device['school_id'], 1);
-                }
-
                 // the desktop app may reconnect on return from hibernation, pc restart etc
                 // therefore the cache maybe be clean but we still need to notify the shelter
                 // about its connection
                 if (!$device['already_triggered'] || $device['device_type'] === 'desktop') {
                     $this->websockets->trigger($device['school_id'], $device['device_id']);
+                }
+                
+                // if it's the first trigger, fire up the events
+                if (!$device['already_triggered'] && $status === Device::TRIGGERED) {
+                    \Event::fire(new AlarmWasTriggered($device['school_id']));
+                    //Set School status
+                    SchoolStatus::statusAlarm($device['school_id'], $status);
+                    // Update client profile, because names are not showed until alarm is not triggered.
+                    $device = Device::where('device_id', '=', $device['device_id'])->get()->first();
+                    $this->websockets->profile(Device::mapDeviceCoordinates($device, $status));
                 }
 
                 $response = ['success' => true, 'calling' => false, 'status' => $status];
