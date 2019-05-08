d31a964379778c80401a9f1f8d9dc04ba0e82f7a
Tadas Planciunas <tadas.planciunas@gmail.com>
2019-05-08 14:30:27 +0300

    Fix

--- database/migrations/2019_05_08_142505_add_fields_to_schools_table2.php
+++ database/migrations/2019_05_08_142505_add_fields_to_schools_table2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToSchoolsTable extends Migration {
+class AddFieldsToSchoolsTable2 extends Migration {
 
 	/**
 	 * Run the migrations.
