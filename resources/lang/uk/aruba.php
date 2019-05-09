8b12518863c94c8aa24b1a4e32c411fbde74efb2
Tadas Plančiūnas <tadas.planciunas@gmail.com>
2019-05-09 11:01:42 +0300

    Master ua (#12)
    * Migrations
    
    * Fix
    
    * Fix
    
    * Schools Model fields
    
    * Ukariane language init
    
    * UK - translation test
    
    * UK Lang: Admin
    
    * Translations UA
    
    * Migration
    
    * MAIL_ENCRYPTION
    
    * Cisco CMX sync: take only associated clients (reduce the load of clients amount from Cisco CMX)

---
+++ resources/lang/uk/aruba.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * BComeSafe, http://bcomesafe.com
+ * Copyright 2015 Magenta ApS, http://magenta.dk
+ * Licensed under MPL 2.0, https://www.mozilla.org/MPL/2.0/
+ * Developed in co-op with Baltic Amadeus, http://baltic-amadeus.lt
+ */
+
+return [
+    'ale' => [
+        'errors' => [
+            'unsynchronized' => 'Немає координат або інформація від шкіл не синхронізується. Будь ласка зачекайте.',
+            'unavailable' => 'Ми не можемо визначити Ваше місцезнаходження. Будь ласка зачекайте.'
+        ]
+    ]
+];
